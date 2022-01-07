712d835b5847ff30d6770e8aa2159113b0ff40f7
Genio07 <93282305+Genio07@users.noreply.github.com>
2022-01-07 15:55:49 -0600

    Update bob.js

--- bob.js
+++ bob.js
@@ -25,7 +25,7 @@ class bob
 			translate(paperpos.x, paperpos.y);
 			rectMode(CENTER)
 			//strokeWeight(4);
-			fill("lightgray");
+			fill("pink");
 			//imageMode(CENTER);
 			//image(this.image, 0,0,this.r, this.r)
 			ellipse(0,0,this.r, this.r);
